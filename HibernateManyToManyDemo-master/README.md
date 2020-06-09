b0d3e1c455a57baaf943da562e836847a0fdf25d
Aneesh Mistry <44435601+4neesh@users.noreply.github.com>
2020-06-09 18:35:19 +0100

    Many to many demo

---
+++ HibernateManyToManyDemo-master/README.md
@@ -0,0 +1,2 @@
+# HibernateManyToManyDemo
+A simple demonstration of using hibernate to create a many to many relationship
---
+++ HibernateManyToManyDemo-master/src/com/aneesh/jdbc/DeleteCourses.java
@@ -0,0 +1,57 @@
+package com.aneesh.jdbc;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.aneesh.hibernate.demo.entity.Course;
+import com.aneesh.hibernate.demo.entity.Instructor;
+import com.aneesh.hibernate.demo.entity.InstructorDetail;
+import com.aneesh.hibernate.demo.entity.Review;
+import com.aneesh.hibernate.demo.entity.Student;
+
+public class DeleteCourses {
+
+	public static void main(String[] args) {
+		
+		//create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+								
+				
+		Session session = factory.getCurrentSession();
+		
+		try {		
+			
+			//start transaction			
+			session.beginTransaction();
+			
+			//get student from database
+			Course a = session.get(Course.class,10);
+
+			session.delete(a);
+			
+		
+			session.getTransaction().commit();
+			
+
+			System.out.println("Done!");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+			factory.close();
+			
+		}
+		
+	}
+	
+}
