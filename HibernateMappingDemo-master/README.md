34f317f6b62a1999ed8b577a51055b48d5745da4
Aneesh Mistry <44435601+4neesh@users.noreply.github.com>
2020-06-09 18:54:23 +0100

    Hibernate Mapping demo (multiple)

---
+++ HibernateMappingDemo-master/README.md
@@ -0,0 +1,2 @@
+# HibernateMappingDemo
+A demonstration of using hibernate with a mySQL database and Java annotations for mapping connections
---
+++ HibernateMappingDemo-master/src/com/aneesh/jdbc/GetInstructorDetailDemo.java
@@ -0,0 +1,59 @@
+package com.aneesh.jdbc;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.aneesh.hibernate.demo.entity.Instructor;
+import com.aneesh.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		//create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+								
+				
+		Session session = factory.getCurrentSession();
+		
+		try {
+					
+			
+			//start transaction			
+			session.beginTransaction();
+			
+			
+			//get instructor detail object
+			int theId = 222;
+			InstructorDetail instructorDetail = session.get(InstructorDetail.class, theId);
+			
+			//print instructor detail object
+			System.out.println("Here is the instructor detail: " + instructorDetail);
+			
+			//print associated instructor item
+			System.out.println("The instructor: " + instructorDetail.getInstructor());
+			
+			
+			//commit instructor
+			session.getTransaction().commit();
+			
+
+			System.out.println("Done!");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+			factory.close();
+			
+		}
+		
+	}
+	
+}
